8763eb04a7d69adf8c0d437ccc15ccedc13cf68f
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2025-04-10 22:01:45 +0300

    Create films.controller.ts

---
+++ backend/src/films/films.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { FilmsService } from './films.service';
+import { ReturnItems } from './util/returnItems';
+import { GetFilmDto } from './dto/get-film.dto';
+import { Schedule } from './repository/schedule.schema';
+import { FindIdParams } from 'src/util/findIdParams';
+import { ReturnError } from 'src/util/returnError';
+
+@Controller('films')
+export class FilmsController {
+  constructor(private readonly filmsService: FilmsService) {}
+
+  @Get('/')
+  async findFilmsAll(): Promise<ReturnItems<GetFilmDto>> {
+    return this.filmsService.findAll();
+  }
+
+  @Get('/:id/schedule')
+  async findFilmScheduleById(
+    @Param() params: FindIdParams,
+  ): Promise<ReturnItems<Schedule> | ReturnError> {
+    return this.filmsService.findScheduleById(params.id);
+  }
+}
