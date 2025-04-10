03b72a3b3a27a10d783eb8dd97d1d440eb80b979
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2025-04-10 22:01:19 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import 'dotenv/config'
+import * as process from 'node:process';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("api/afisha");
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix(process.env.PREFIX || 'api/afisha');
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
