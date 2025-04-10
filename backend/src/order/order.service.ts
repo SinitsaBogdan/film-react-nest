bff0942cf14450a4be09657524be3ba079db42bf
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2025-04-10 22:01:03 +0300

    Create order.service.ts

---
+++ backend/src/order/order.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@nestjs/common';
+import { FilmsRepository } from 'src/films/repository/films.repository';
+import { OrdersRepository } from 'src/order/repository/orders.repository';
+import { CreateOrderDto, ReturnOrdersDto } from './dto';
+import { ReturnError } from 'src/util/returnError';
+
+@Injectable()
+export class OrderService {
+  constructor(
+    private readonly filmsRepository: FilmsRepository,
+    private readonly ordersRepository: OrdersRepository,
+  ) {}
+
+  async createOrder(
+    createOrderDto: Omit<CreateOrderDto, 'id'>,
+  ): Promise<ReturnOrdersDto | ReturnError> {
+    try {
+      const films = [];
+      for (const film of createOrderDto.tickets) {
+        const result = await this.filmsRepository.findFilmById(film.film);
+        films.push(result);
+      }
+
+      for (const film of films) {
+        for (const schedule of film.schedule) {
+          for (const ticket of createOrderDto.tickets) {
+            if (
+              schedule.id === ticket.session &&
+              schedule.daytime === ticket.daytime &&
+              schedule.taken.find(
+                (item) => item === `${ticket.row}:${ticket.seat}`,
+              )
+            ) {
+              throw new Error('Билеты с такими данными уже существует');
+            }
+          }
+        }
+      }
+      const result = await this.ordersRepository.createOrder(createOrderDto);
+
+      for (const ticket of createOrderDto.tickets) {
+        const taken = `${ticket.row}:${ticket.seat}`;
+        const filmItem = films.find((item) => item.id === ticket.film);
+        filmItem.schedule.forEach((element) => {
+          if (
+            element.id === ticket.session &&
+            element.daytime === ticket.daytime
+          ) {
+            element.taken.push(taken);
+          }
+        });
+        await this.filmsRepository.updateFilmScheduleById(
+          ticket.film,
+          filmItem.schedule,
+        );
+      }
+      return {
+        total: result.tickets.length,
+        items: result.tickets,
+      };
+    } catch (e) {
+      return { error: e.message };
+    }
+  }
+}
