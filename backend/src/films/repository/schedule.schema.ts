d9769f2183f8d876ae0be96ab450cf1c29750e9a
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2025-04-10 22:01:33 +0300

    Create schedule.schema.ts

---
+++ backend/src/films/repository/schedule.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+
+@Schema()
+export class Schedule {
+  @Prop({ required: true })
+  id: string;
+
+  @Prop({ required: true })
+  daytime: string;
+
+  @Prop({ required: true })
+  hall: number;
+
+  @Prop({ required: true })
+  rows: number;
+
+  @Prop({ required: true })
+  seats: number;
+
+  @Prop({ required: true })
+  price: number;
+
+  @Prop()
+  taken: [];
+}
