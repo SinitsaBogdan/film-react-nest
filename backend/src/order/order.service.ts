23dbdc95b104c992fadb21ce769c29065f3a93c6
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2025-04-26 12:11:47 +0300

    Update order.service.ts

--- backend/src/order/order.service.ts
+++ backend/src/order/order.service.ts
@@ -39,22 +39,11 @@ export class OrderService {
       }
       const result = await this.ordersRepository.createOrder(createOrderDto);
 
-      for (const ticket of createOrderDto.tickets) {
-        const taken = `${ticket.row}:${ticket.seat}`;
-        const filmItem = films.find((item) => item.id === ticket.film);
-        filmItem.schedule.forEach((element) => {
-          if (
-            element.id === ticket.session &&
-            element.daytime === ticket.daytime
-          ) {
-            element.taken.push(taken);
-          }
-        });
-        await this.filmsRepository.updateFilmScheduleById(
-          ticket.film,
-          filmItem.schedule,
-        );
-      }
+      await this.filmsRepository.updateFilmSchedules(
+        createOrderDto.tickets,
+        films,
+      );
+
       return {
         total: result.tickets.length,
         items: result.tickets,
